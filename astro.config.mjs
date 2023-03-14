ca81367b4e6f61f0b09962a838a372b919f4beb9
Yuri Gomes <yuurig@gmail.com>
2023-03-14 11:59:27 -0300

    chore: new site

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
+import tailwind from '@astrojs/tailwind';
+
+import robotsTxt from 'astro-robots-txt';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    compress({
+      js: true,
+      css: true,
+      html: true
+    }),
+    tailwind(),
+    robotsTxt()
+  ],
+  site: "https://met4tron.github.io",
+  base: "/met4tron.github.io",
+});
