256dc290ed7dedd1ffc1d1aa68633c5f43d00595
Yuri Gomes <yuurig@gmail.com>
2023-03-14 20:12:01 -0300

    chore: more config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
 import { defineConfig } from 'astro/config';
 import compress from 'astro-compress';
 import tailwind from '@astrojs/tailwind';
-
 import robotsTxt from 'astro-robots-txt';
+
+import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    compress({
-      js: true,
-      css: true,
-      html: true
-    }),
-    tailwind(),
-    robotsTxt()
-  ],
-  site: "https://met4tron.github.io",
+  integrations: [compress({
+    js: true,
+    css: true,
+    html: true
+  }), tailwind(), robotsTxt(), critters()],
+  site: "https://met4tron.github.io"
 });
